6df6b38835465d43febce601e8d51223cc5c196a
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2023-10-17 20:39:49 +0530

    Initial commit

---
+++ scripts/postInstall.sh
@@ -0,0 +1,8 @@
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+#wait until the server is ready
+echo "Waiting for software to be ready ..."
+sleep 250s;
+
+echo 'y' | docker-compose exec -T pleroma ./cli.sh user new admin ${ADMIN_EMAIL} --admin --password ${ADMIN_PASSWORD}
---
+++ scripts/preInstall.sh
@@ -0,0 +1,72 @@
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+cat <<EOT > ./servers.json
+{
+    "Servers": {
+        "1": {
+            "Name": "local",
+            "Group": "Servers",
+            "Host": "172.17.0.1",
+            "Port": 57249,
+            "MaintenanceDB": "postgres",
+            "SSLMode": "prefer",
+            "Username": "postgres",
+            "PassFile": "/pgpass"
+        }
+    }
+}
+EOT
+
+
+cat /opt/elestio/startPostfix.sh > post.txt
+filename="./post.txt"
+
+SMTP_LOGIN=""
+SMTP_PASSWORD=""
+
+while IFS= read -r line; do
+  values=$(echo "$line" | grep -o '\-e [^ ]*' | sed 's/-e //')
+
+  while IFS= read -r value; do
+    if [[ $value == RELAYHOST_USERNAME=* ]]; then
+      SMTP_LOGIN=${value#*=}
+    elif [[ $value == RELAYHOST_PASSWORD=* ]]; then
+      SMTP_PASSWORD=${value#*=}
+    fi
+  done <<< "$values"
+
+done < "$filename"
+
+cat <<EOT > ./config.exs
+
+import Config
+
+config :pleroma, :instance,
+  registrations_open: true,
+  invites_enabled: true,
+  federating: true,
+  allow_relay: true
+
+config :pleroma, Pleroma.Emails.Mailer,
+  enabled: true,
+  adapter: Swoosh.Adapters.SMTP,
+  relay: "tuesday.mxrouting.net",
+  username: "${SMTP_LOGIN}",
+  password: "${SMTP_PASSWORD}",
+  port: 25,
+  ssl: false,
+  auth: :always
+
+config :pleroma, configurable_from_database: true
+
+config :pleroma, :media_proxy,
+      enabled: true,
+      proxy_opts: [
+            redirect_on_failure: true
+      ],
+      base_url: "https://${DOMAIN}"
+
+EOT
+
+rm post.txt
